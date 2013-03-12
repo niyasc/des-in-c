13593f85e08df69b3168cd44b045fc44ba6e5397
niyasc <niyasmonc@gmail.com>
2013-03-12 12:44:58 +0530

    Encryption completed, ignored initial permutation and reverse initial permutation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 des-in-c
 ========
 
-This will provide program to encrypt message,decrypt and to generate random encryption keys and shuffling
+This will provide program to encrypt message,decrypt and to generate random encryption keys and shuffling. Initial permutation and final reverse permutation has ignored.
--- encrypt.c
+++ encrypt.c
@@ -46,6 +46,57 @@ const short int p[32]=	{
 			0,14,22,25,4,17,30,9,
 			1,7,23,13,31,26,2,8,
 			18,12,29,5,21,10,3,24
+			};
+const short int s[8][4][16]={
+				{
+					{14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},
+					{0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8},
+					{4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0},
+					{15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13}
+				},
+				{
+					{15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10},
+					{3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5},
+					{0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15},
+					{13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9}
+				},
+				{
+					{10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8},
+					{13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1},
+					{13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7},
+					{1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12}
+				},
+				{
+					{7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15},
+					{13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9},
+					{10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4},
+					{3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14},
+				},
+				{
+					{2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9},
+					{14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6},
+					{4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14},
+					{11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3},
+				},
+				{
+					{12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11},
+					{10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8},
+					{9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6},
+					{4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13}
+				},
+				{
+					{4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1},
+					{13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6},
+					{1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2},
+					{6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12},
+				},
+				{
+					{13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7},
+					{1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2},
+					{7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8},
+					{2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11},
+				}
+			
 			};			
 
 void cls(bool key[],int c)
@@ -78,9 +129,9 @@ bool xor(bool a,bool b)
 		
 int main(int argc,char *argv[])
 {
-	if(argc!=3)
+	if(argc!=4)
 	{
-		printf("Syntax: <executable> <input file> <key>\n");
+		printf("Syntax: <executable> <input file> <key> <output file>\n");
 		return 1;
 	}
 	if(strlen(argv[2])<8)
@@ -124,38 +175,11 @@ int main(int argc,char *argv[])
 	{
 		key_permuted[i]=key_bin[pc1[i]];
 	}
-	printf("\nKey after pc1(56) : \n");
-	printf("C-1 : ");
-	for(i=0;i<28;i++)
-		printf("%d",key_permuted[i]);
-	printf("\nD-1 : ");
-	for(i=28;i<56;i++)
-		printf("%d",key_permuted[i]);
-	printf("\nKey generation process\n");
 	for(i=0;i<16;i++)
 	{
-		printf("\nRound %d\n",i);
 		cls(key_permuted,cls_value[i]);
-		printf("Key after  %d cls : \n",cls_value[i]);
-		printf("C%d  : ",i);
-		for(j=0;j<28;j++)
-			printf("%d",key_permuted[j]);
-		printf("\nD%d  : ",i);
-		for(j=28;j<56;j++)
-			printf("%d",key_permuted[j]);
 		for(j=0;j<48;j++)
 			k[i][j]=key_permuted[pc2[j]];
-	}
-	printf("\n48 bit keys are\n");
-	for(i=0;i<16;i++)
-	{
-		printf("\nRound %d \n",i);
-		for(j=0;j<48;j++)
-		{
-			if(j&&!(j%8))
-				printf(" ");
-			printf("%d",k[i][j]);
-		}
 	}
 	//read message from file
 	char message[1024];
@@ -169,8 +193,9 @@ int main(int argc,char *argv[])
 	i=0;
 	while(!feof(ptr))
 		fscanf(ptr,"%c",&message[i++]);
-	message[i]=0;
-	printf("Message text : \n%s[eom]\n",message);
+	message[i-1]=0;
+	fclose(ptr);
+	printf("\nMessage text : \n%s[eom]\n",message);
 	//perform padding
 	if(strlen(message)%8!=0)
 	{
@@ -182,14 +207,17 @@ int main(int argc,char *argv[])
 		message[i]=0;
 		printf("Message after padding : \n%s\n",message);
 	}
+	ptr=fopen(argv[3],"w");
 	for(i=0;i<strlen(message)/8;i++)
 	{
 		bool m[64];
 		for(j=0;j<64;j++)
 			m[j]=0;
+		printf("Message block : ");
 		for(j=i*8;j<(i+1)*8;j++)
 		{
 			int t=message[j];
+			printf("%c",t);
 			int k=(j-i*8)*8+7;
 			while(t)
 			{
@@ -198,23 +226,27 @@ int main(int argc,char *argv[])
 				k--;
 			}
 		}
-		//debugging print
 		printf("\n");
-		for(j=i*8;j<(i+1)*8;j++)
-			printf("%c",message[j]);
-		printf(" ");
-		for(j=0;j<64;j++)
-		{
-			if(j&&!(j%8))
-				printf(" ");
-			printf("%d",m[j]);
-		}
+		//debugging print
 		//perform conversion
 		for(j=0;j<16;j++)
 		{
-			printf("Round 1\nMessage\n");
-			bool mr[48];
+			printf("Round %d\n",j+1);
 			int l=0;
+			printf("Key              :");
+			for(l=0;l<48;l++)
+				printf("%d",k[j][l]);
+			printf("\n");
+			printf("Message in binary:");
+			for(l=0;l<64;l++)
+			{
+				if(l&&!(l%32))
+					printf(" ");
+				printf("%d",m[l]);
+			}
+			printf("\n");
+			bool mr[48];
+			
 			for(l=0;l<48;l++)
 			{
 				mr[l]=m[e[l]];
@@ -222,12 +254,44 @@ int main(int argc,char *argv[])
 			for(l=0;l<48;l++)
 				mr[l]=xor(mr[l],k[j][l]);
 			bool t[32];
+			for(l=0;l<8;l++)
+			{
+				bool r[6];//input of sbox;
+				int a;
+				for(a=l*6;a<(l+1)*6;a++)
+					r[a-l*6]=mr[a];
+				bool o[4];//output of sbox;
+				int x=r[0]*2+r[5];
+				int y=0;
+				int b;
+				for(b=1;b<5;b++)
+					y=y*2+r[b];
+				int z=s[l][x][y];
+				for(b=0;b<4;b++)
+					o[b]=0;
+				b=3;
+				while(z)
+				{
+					o[b]=z%2;
+					z=z/2;
+					b--;
+				}
+				for(b=l*4;b<(l+1)*4;b++)
+				{
+					t[b]=o[b-l*4];
+				}
+			}
 			for(l=0;l<32;l++)
-				t[l]=
-	}
-		
-
-		
+			{
+				m[l]=m[l+32];
+			}
+			for(l=0;l<32;l++)
+				m[l+32]=t[l];
+		}
+		for(j=0;j<64;j++)
+			fprintf(ptr,"%d",m[j]);			
+	}	
+	fclose(ptr);
 	printf("\n");
 	return 0;
 }
